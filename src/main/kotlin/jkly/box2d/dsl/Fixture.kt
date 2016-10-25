5421cd68d5ec86f3c6ad3b95bb0b29cfb897b378
Jingkei Ly <jkly25@gmail.com>
2016-10-26 00:18:23 +0100

    use delegate for setting fixtures

--- src/main/kotlin/jkly/box2d/dsl/Fixture.kt
+++ src/main/kotlin/jkly/box2d/dsl/Fixture.kt
@@ -2,6 +2,7 @@ package jkly.box2d.dsl
 
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.*
+import kotlin.reflect.KProperty
 
 
 fun BodyFixtureDsl.withCircle(configure: CircleFixtureDsl.() -> Unit): Fixture {
@@ -34,6 +35,38 @@ fun circle(configure: CircleFixtureDsl.()->Unit): FixtureDef {
 }
 
 abstract class FixtureDsl(private val fixtureDef: FixtureDef) {
+    val filter: Filter
+    init {
+        filter = fixtureDef.filter
+    }
+    var friction: Float by object {
+        operator fun getValue(dsl: FixtureDsl, property: KProperty<*>): Float = fixtureDef.friction
+        operator fun setValue(dsl: FixtureDsl, property: KProperty<*>, friction: Float) {
+            fixtureDef.friction = friction
+        }
+    }
+
+    var restitution: Float by object {
+        operator fun getValue(dsl: FixtureDsl, property: KProperty<*>): Float = fixtureDef.restitution
+        operator fun setValue(dsl: FixtureDsl, property: KProperty<*>, restitution: Float) {
+            fixtureDef.restitution = restitution
+        }
+    }
+
+    var density: Float by object {
+        operator fun getValue(dsl: FixtureDsl, property: KProperty<*>): Float = fixtureDef.density
+        operator fun setValue(dsl: FixtureDsl, property: KProperty<*>, density: Float) {
+            fixtureDef.density = density
+        }
+    }
+
+    var isSensor: Boolean by object {
+        operator fun getValue(dsl: FixtureDsl, property: KProperty<*>): Boolean = fixtureDef.isSensor
+        operator fun setValue(dsl: FixtureDsl, property: KProperty<*>, isSensor: Boolean) {
+            fixtureDef.isSensor = isSensor
+        }
+    }
+
     fun friction(friction:Float) {
         fixtureDef.friction = friction
     }
@@ -49,8 +82,6 @@ abstract class FixtureDsl(private val fixtureDef: FixtureDef) {
     fun isSensor(isSensor: Boolean) {
         fixtureDef.isSensor = isSensor
     }
-
-    val filter = fixtureDef.filter
 }
 
 class CircleFixtureDsl(val circle: CircleShape, fixtureDef: FixtureDef) : FixtureDsl(fixtureDef) {
