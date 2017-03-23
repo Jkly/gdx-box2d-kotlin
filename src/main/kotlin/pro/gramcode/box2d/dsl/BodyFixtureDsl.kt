9c619d9471001a4ed2a68626a65ea5b55e09d824
Jingkei Ly <jkly25@gmail.com>
2017-03-23 15:36:21 +0000

    added callback function for handling fixtures created

--- src/main/kotlin/pro/gramcode/box2d/dsl/BodyFixtureDsl.kt
+++ src/main/kotlin/pro/gramcode/box2d/dsl/BodyFixtureDsl.kt
@@ -7,16 +7,16 @@ import com.badlogic.gdx.physics.box2d.Shape
 
 @Box2dDslMarker
 class BodyFixtureDsl(val body: Body) {
-    fun with(fixtureDef: FixtureDef): Fixture {
-        return addFixture(fixtureDef.shape, body, fixtureDef)
+    fun with(fixtureDef: FixtureDef, fixtureHandler: (Fixture) -> Unit = {}) {
+        fixtureHandler(addFixture(fixtureDef.shape, body, fixtureDef))
     }
 
-    fun circle(configure: CircleFixtureDsl.()->Unit = {}): Fixture {
-        return with(circleDef(configure))
+    fun circle(fixtureHandler: (Fixture)->Unit = {}, configure: CircleFixtureDsl.()->Unit = {}) {
+        return with(circleDef(configure), fixtureHandler)
     }
 
-    fun polygon(configure: PolygonFixtureDsl.()->Unit = {}): Fixture {
-        return with(polygonDef(configure))
+    fun polygon(fixtureHandler: (Fixture)->Unit = {}, configure: PolygonFixtureDsl.()->Unit = {}) {
+        return with(polygonDef(configure), fixtureHandler)
     }
 
     private fun <T: Shape> addFixture(shape: T, body: Body, fixtureDef: FixtureDef): Fixture {
