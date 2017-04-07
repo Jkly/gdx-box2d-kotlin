5eb18a2f5d10738b2342cbec1b1b9ded9f69b5cb
Jingkei Ly <jkly25@gmail.com>
2017-04-07 17:46:49 +0100

    change so you don't have to specify the angle

--- src/main/kotlin/pro/gramcode/box2d/dsl/PolygonFixtureDsl.kt
+++ src/main/kotlin/pro/gramcode/box2d/dsl/PolygonFixtureDsl.kt
@@ -5,7 +5,7 @@ import com.badlogic.gdx.physics.box2d.FixtureDef
 import com.badlogic.gdx.physics.box2d.PolygonShape
 
 class PolygonFixtureDsl(val polygon: PolygonShape, fixtureDef: FixtureDef) : FixtureDsl(fixtureDef) {
-    fun setAsBox(hx: Float, hy: Float, center: Vector2, angle: Float) {
+    fun setAsBox(hx: Float, hy: Float, center: Vector2, angle: Float = 0f) {
         polygon.setAsBox(hx, hy, center, angle)
     }
     fun setAsBox(hx: Float, hy: Float) {
