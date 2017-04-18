92cc606ea8518fe1af7d7bb62fda01b0390d1eb8
Jingkei Ly <jkly25@gmail.com>
2017-04-18 18:04:30 +0100

    remove unnecessary use of property delegates

--- src/main/kotlin/pro/gramcode/box2d/dsl/CircleFixtureDsl.kt
+++ src/main/kotlin/pro/gramcode/box2d/dsl/CircleFixtureDsl.kt
@@ -3,22 +3,19 @@ package pro.gramcode.box2d.dsl
 import com.badlogic.gdx.math.Vector2
 import com.badlogic.gdx.physics.box2d.CircleShape
 import com.badlogic.gdx.physics.box2d.FixtureDef
-import kotlin.reflect.KProperty
 
 class CircleFixtureDsl(val circle: CircleShape, fixtureDef: FixtureDef) : FixtureDsl(fixtureDef) {
-    var position: Vector2 by object {
-        operator fun getValue(dsl: CircleFixtureDsl, property: KProperty<*>): Vector2 = circle.position
-        operator fun setValue(dsl: CircleFixtureDsl, property: KProperty<*>, position: Vector2) {
+    var position: Vector2
+        get() = circle.position
+        set(position) {
             circle.position = position
         }
-    }
 
-    var radius: Float by object {
-        operator fun getValue(dsl: FixtureDsl, property: KProperty<*>): Float = circle.radius
-        operator fun setValue(dsl: FixtureDsl, property: KProperty<*>, radius: Float) {
+    var radius: Float
+        get() = circle.radius
+        set(radius) {
             circle.radius = radius
         }
-    }
 
     fun position(pos: Vector2) {
         circle.position = pos
